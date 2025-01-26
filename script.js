0a824696308579669beb675a83a90b82bc3ab0b9
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-01-26 17:12:26 +0100

    creating a function that accepts the country and displays the info

--- script.js
+++ script.js
@@ -13,17 +13,18 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-// old way of wrting AJAX
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v2/name/portugal');
-request.send();
+const getCountryData = function (country) {
+  // old way of wrting AJAX
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
 
-request.addEventListener('load', function () {
-  //  converting the data form JSON to javascript object
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+  request.addEventListener('load', function () {
+    //  converting the data form JSON to javascript object
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const html = `
+    const html = `
           <article class="country">
             <img class="country__img" src="${data.flag}"/>
             <div class="country__data">
@@ -42,35 +43,10 @@ request.addEventListener('load', function () {
         </article>
         `;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-});
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
 
-const request2 = new XMLHttpRequest();
-request2.open('GET', 'https://restcountries.com/v2/name/nigeria');
-request2.send();
-
-request2.addEventListener('load', function () {
-  const [data2] = JSON.parse(this.responseText);
-
-  const html2 = `
-    <article class="country">
-        <img class="country__img" src="${data2.flag}"/>
-        <div class="country__data">
-            <h3 class="country__name">${data2.name}</h3>
-            <h4 class="country__region">${data2.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data2.population / 1000000
-            ).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${
-              data2.languages[0].name
-            }</p>
-            <p class="country__row"><span>💰</span>${
-              data2.currencies[0].name
-            }</p>
-        </div>
-    </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html2);
-  console.log(data2);
-});
+getCountryData('portugal');
+getCountryData('nigeria');
