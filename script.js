639a38466ae04a79e0349e7749654f24a61481cb
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-01-26 17:08:11 +0100

    displaying info about the country in the browser

--- script.js
+++ script.js
@@ -20,8 +20,30 @@ request.send();
 
 request.addEventListener('load', function () {
   //  converting the data form JSON to javascript object
-  const data = JSON.parse(this.responseText);
+  const [data] = JSON.parse(this.responseText);
   console.log(data);
+
+  const html = `
+          <article class="country">
+            <img class="country__img" src="${data.flag}"/>
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+          </div>
+        </article>
+        `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
 
 const request2 = new XMLHttpRequest();
@@ -30,5 +52,25 @@ request2.send();
 
 request2.addEventListener('load', function () {
   const [data2] = JSON.parse(this.responseText);
+
+  const html2 = `
+    <article class="country">
+        <img class="country__img" src="${data2.flag}"/>
+        <div class="country__data">
+            <h3 class="country__name">${data2.name}</h3>
+            <h4 class="country__region">${data2.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data2.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${
+              data2.languages[0].name
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              data2.currencies[0].name
+            }</p>
+        </div>
+    </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html2);
   console.log(data2);
 });
