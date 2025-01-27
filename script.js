d29518b632ebbe73e19deb0900441c1d9e672752
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-01-27 23:20:35 +0100

    refractoring getcountrydata function to use an arrow function

--- script.js
+++ script.js
@@ -144,16 +144,24 @@ const renderCountry = function (data, className = '') {
 const request = fetch('https://restcountries.com/v2/name/portugal');
 console.log(request);
 
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+// getCountryData('portugal');
+
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      renderCountry(data[0]);
-    });
+  const request = fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
 };
 
 getCountryData('portugal');
