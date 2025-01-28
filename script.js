b0ce5e123d2da19e7c75d03c8c427589e135622a
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-01-28 21:39:18 +0100

    Challenge#1 solution

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ const renderCountry = function (data, className = '') {
               `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
@@ -219,6 +219,8 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 //   getNeighbouringCountry('portugal');
 // });
 
+/*
+
 const getNeighbouringCountry = function (country) {
   // country 1
   getJSON(`https://restcountries.com/v2/name/${country}`, `Country not found`)
@@ -249,3 +251,76 @@ btn.addEventListener('click', function () {
 });
 
 // getNeighbouringCountry('aaaaaaa');
+
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.
+The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=52.508&longitude=13.381. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+/*
+
+// Solution
+
+// Part 1
+// 1.
+const whereAmI = function (lat, lng) {
+  const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`;
+  // 2.
+  fetch(url)
+    .then(response => {
+      console.log(response);
+      if (!response.ok) throw new Error('Somethng went wrong');
+      return response.json();
+    })
+    .then(data => {
+      // 3
+      const message = `You are in ${data.city}, ${data.countryName}`;
+      console.log(message);
+      // Part 2
+      return fetch(`https://restcountries.com/v2/name/${data.countryName}`);
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Something went wrong ${response.status}`);
+      return response.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => {
+      // 4
+      console.log(`${err.message}`);
+    });
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+console.log(whereAmI(-33.933, 18.474));
+
+btn.addEventListener('click', function () {
+  getCountryData(whereAmI(52.508, 13.381));
+});
+
+*/
